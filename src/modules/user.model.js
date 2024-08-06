5391bffdc09afd8403457a416e925d9e9c1484e2
matrixeye0 <singhjagjit9735@gmail.com>
2024-08-06 15:40:41 +0530

    USER AND VIDEO FILE

--- src/modules/user.model.js
+++ src/modules/user.model.js
@@ -1,6 +1,8 @@
 import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
+
+
 const userSchema = new Schema({
 username:{
     type:String,
@@ -45,5 +47,45 @@ refreshToken:{
     type:String,
 }
 })
+
+userSchema.pre("save",async function (next){
+    if(!this.isModified("password")) return next();
+
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password)
+{
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.method.generateAccessToken= function(){
+return jwt.sign({
+    _id:this._id,
+    email:this.email,
+    username:this.username,
+    fullname:this.fullname
+},
+process.env.ACCESS_TOKEN_SECRET,
+{
+    expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+}
+)
+}
+
+userSchema.method.generateRefreshToken= function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+    )
+}
 
 export const User = mongoose.model("user",userSchema)
